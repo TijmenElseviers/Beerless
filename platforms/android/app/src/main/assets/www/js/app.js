0c3bc93684ce5c68e5d37c88a7899a11ce5b4c95
TijmenElseviers <tijmenelseviers@gmail.com>
2018-03-19 16:36:36 +0100

    Project bij de start

---
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -0,0 +1,13 @@
+$(function(){
+    document.addEventListener("deviceready", onDeviceReady, false);
+    $('.button-collapse').sideNav();
+    $('.side-nav a').click(function(){
+        $('.spa').hide();
+        $('#' + $(this).data('show')).show();
+        $('.button-collapse').sideNav('hide');
+    });
+});
+
+function onDeviceReady() {
+    console.log('Device is ready');
+};
